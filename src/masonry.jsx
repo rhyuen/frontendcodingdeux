4235277ccab89889d9abc995258e1293b90efcbb
Robert Yuen <dungeonry@hotmail.com>
2017-10-06 17:36:31 -0700

    Added state and props for images.

--- src/masonry.jsx
+++ src/masonry.jsx
@@ -1,39 +1,59 @@
 import React, {Component} from "react";
 import img from "./images/spiderman.jpg";
 
+
 const MsnItem = ({imgReference}) => (
     <div className ="msn__row__item">        
         <img src = {imgReference}/>
         <div className = "msn__row__item__social">
-            <div clasName = "msn__row__item__social__icon">A</div>
-            <div clasName = "msn__row__item__social__icon">B</div>
-            <div clasName = "msn__row__item__social__icon">B</div>
+            <div className = "msn__row__item__social__icon">X</div>
+            <div className = "msn__row__item__social__icon">^</div>
+            <div className = "msn__row__item__social__icon">V</div>
         </div>            
     </div>    
 );
 
-const MsnRow = () => {
-    const images = require.context("./images", false, /\.(png|jpg)$/);
-    const handled = images.keys().map(images);
-
-    const list = handled.map((item, index) => {
-        return <MsnItem imgReference = {item} key = {index}/>
-    });
-
-    return (
-        <div className = "msn__row">
-            {list}
-        </div>
-    );
-};
+const MsnRow = ({gallery}) => (
+    <div className = "msn__row">
+        {
+            gallery.map((item, index) => <MsnItem imgReference = {item.imageLink} key = {index}/>)
+        }
+    </div>
+);
 class Masonry extends Component{
     state = {
+        pictures: []
+    }
 
+    componentDidMount= () => {
+        const images = require.context("./images", false, /\.(png|jpg)$/);
+        const handled = images.keys().map(images);
+        const imageList = handled.map((currImage) => {
+            return {
+                imageLink: currImage,
+                visible: true,
+                likes: 0
+            };
+        });
+
+        this.setState((prevState) => {
+            return {
+                pictures: imageList
+            };
+        });
+    }
+
+    handleVisibilityChange = () => {
+
+    }
+
+    handleUpvote = () => {
+        
     }
     render(){
         return (
-            <div className = "msn">
-                <MsnRow/>
+            <div className = "msn">                
+                <MsnRow gallery = {this.state.pictures}/>
             </div>
         );
     }
