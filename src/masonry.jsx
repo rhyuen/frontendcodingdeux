0cce67ec2d4d19138122193c74063d10150ce613
Robert Yuen <dungeonry@hotmail.com>
2017-10-17 20:41:29 -0700

    Fixed the issue with close image handler.  Forgot to return the entire state object.

--- src/masonry.jsx
+++ src/masonry.jsx
@@ -1,31 +1,35 @@
 import React, {Component} from "react";
-import img from "./images/spiderman.jpg";
 import uuid from "uuid";
 
 
-const MsnItem = ({imgReference, onImageClose, id}) => (
+const MsnItem = ({imgReference, onImageClose, id, likes, onImageUpvote, onImageDownvote}) => (
     <div className ="msn__row__item">        
         <img src = {imgReference}/>
         <div className = "msn__row__item__social">
             <div className = "msn__row__item__social__icon" onClick = {onImageClose.bind(this, id)}>X</div>
-            <div className = "msn__row__item__social__icon">^</div>
-            <div className = "msn__row__item__social__icon">V</div>
+            <div className = "msn__row__item__social__icon" onClick = {onImageUpvote.bind(this, id)}>^</div>
+            <div className = "msn__row__item__social__icon" onClick = {onImageDownvote.bind(this, id)}>V</div>
         </div>            
+        <div className = "msn__row__item__summary">{likes} Likes</div>
     </div>    
 );
 
-const MsnRow = ({gallery, onImageClose}) => (
+const MsnRow = ({gallery, onImageClose, onImageUpvote, onImageDownvote}) => (
     <div className = "msn__row">
         {
             gallery.map((item) => {
                 if(item.visible){
                     return <MsnItem imgReference = {item.imageLink} 
                         key = {item.id} 
-                        id = {item.id} 
-                        onImageClose = {onImageClose}/>;
+                        id = {item.id}
+                        likes = {item.likes} 
+                        onImageClose = {onImageClose}
+                        onImageUpvote = {onImageUpvote}
+                        onImageDownvote = {onImageDownvote}/>;
                 }
-            })
+            })            
         }
+        <MsnFooter/>
     </div>
 );
 class Masonry extends Component{
@@ -64,30 +68,85 @@ class Masonry extends Component{
             }       
             const updatedImg = Object.assign({},
                 prevState.pictures[index], 
-                {visible: !prevState.pictures[index].visible}
-            );
-            
-            const beginning = prevState.pictures.slice(0, index);
-            console.log(beginning);
-            const end = prevState.pictures.splice(index + 1);                
-            console.log(end);
-            const updatedlist = beginning.concat(updatedImg).concat(end);
-            console.log(updatedlist);
-            return updatedlist;
+                {visible: false}
+            );            
+            const beginning = prevState.pictures.slice(0, index);            
+            const end = prevState.pictures.splice(index + 1);                            
+            const updatedlist = beginning.concat(updatedImg).concat(end);            
+            return {
+                pictures: updatedlist
+            };
                 
         });
     }
 
-    handleUpvote = () => {
+    handleUpvote = (id) => {
+        let index = null;
+        for(let i = 0; i < this.state.pictures.length; i++){
+            if(this.state.pictures[i].id === id){
+                index = i;
+            }
+        }
+                
+        this.setState(prevState => {
+            const updatedImg = Object.assign({}, 
+                this.state.pictures[index], 
+                {likes: prevState.pictures[index].likes + 1}
+            );
 
+            return {
+                pictures: prevState.pictures.slice(0, index)
+                    .concat(updatedImg)
+                    .concat(prevState.pictures.splice(index + 1))
+            };
+        });
+    }
+
+    handleDownVote = (id) => {
+        let index = null;
+        for(let i = 0; i < this.state.pictures.length; i++){
+            if(this.state.pictures[i].id === id){
+                index = i;
+            }
+        }
+                
+        this.setState(prevState => {
+            const updatedImg = Object.assign({}, 
+                this.state.pictures[index], 
+                {likes: prevState.pictures[index].likes - 1}
+            );
+
+            return {
+                pictures: prevState.pictures.slice(0, index)
+                    .concat(updatedImg)
+                    .concat(prevState.pictures.splice(index + 1))
+            };
+        });
     }
     render(){
         return (
             <div className = "msn">                
-                <MsnRow gallery = {this.state.pictures} onImageClose = {this.handleVisibilityChange}/>
-            </div>
+                <MsnRow gallery = {this.state.pictures} 
+                    onImageClose = {this.handleVisibilityChange}
+                    onImageUpvote = {this.handleUpvote}
+                    onImageDownvote = {this.handleDownVote}/>                
+            </div>            
         );
     }
 }
+
+const MsnViewer = () => (
+    <div>
+        Viewer
+    </div>
+);
+
+const MsnFooter = () => (
+    <div className ="msn__footer">  
+        <div className = "msn__footer__content">
+            <span>That's all for now.</span>
+        </div>        
+    </div>
+);
 
 export default Masonry;
